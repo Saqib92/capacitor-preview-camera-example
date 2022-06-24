483e3d764e1041062cd0795eec3ff9ea325383d8
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-06-24 17:57:29 +0500

    android and ios setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.saqib.cam',
   appName: 'ExampleCam',
   webDir: 'www',
   bundledWebRuntime: false
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PreviewCamera } from '@numbersprotocol/preview-camera';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  recording: boolean = false;
+
+  constructor() {
+
+  }
+  
+  ngOnInit() {
+    this.startCam();
+    PreviewCamera.addListener(
+      'capturePhotoFinished',
+      (res)=>{
+        console.log(res)
+      }
+    ).then((e:any)=>{
+      console.log(e)
+    })
+  }
+
+
+  async startCam(){
+     let res = await PreviewCamera.startPreview()
+     console.log('preview', res)
+  }
+
+  async recordCam(){
+    this.recording = true;
+    let res = await PreviewCamera.startRecord();
+    console.log('start' , res)
+
+  }
+
+  async stopCam(){
+    this.recording = false;
+    let res = await PreviewCamera.stopRecord()
+    console.log(res);
+  }
 
 }
